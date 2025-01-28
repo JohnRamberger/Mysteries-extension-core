5aedf43471ee6fa33613bfd29fea2484e6c0024b
John Ramberger <rambergerjohn@gmail.com>
2025-01-27 22:39:22 -0500

    fix versions

--- Extension-core/build.gradle.kts
+++ Extension-core/build.gradle.kts
@@ -12,8 +12,13 @@ android {
     defaultConfig {
         minSdk = 21
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+    }
+
+    sourceSets {
+        named("main") {
+            java.srcDirs("src")
+        }
     }
 
     buildTypes {
@@ -41,19 +46,11 @@ android {
     }
 }
 
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
-
 ktlint {
     android = true // to use the Android Studio KtLint plugin style
     ignoreFailures = false
 }
+
 project.afterEvaluate {
     publishing {
         publications {
